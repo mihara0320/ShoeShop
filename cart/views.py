2646a106cb960e7bd9de3eff746ccfff38a1cf88
Masaki Ihara <mihara@klass2-06-l.intra.itcollege.ee>
2017-05-02 17:33:05 +0300

    cart refresh after checkout

--- cart/views.py
+++ cart/views.py
@@ -4,43 +4,54 @@ from showcase.models import Item
 def content_view(request):
     model = Item
     template_name = 'cart/content.html'
-    queryset = model.objects.all()
     items_in_cart = []
     total_price = 0
+    checkout_price = 0
     message = None
-
+    context = { 'Item':model }
 
     try:
         cart = request.session.get('cart')
+    except:
+        print("Cart does not exist")
+
+    if request.method == 'GET':
+        print('get')
         if (len(cart) == 0):
             message = 'Your cart is empty'
+            context.update({
+                'total_price': total_price,
+                'message':message, 
+            })
         else:
             for key in cart:
                 query = model.objects.get(pk = key)
                 total_price += query.price
-                print(total_price)
                 items_in_cart.append(query)
-    except:
-        print("Cart does not exist")
-        message = 'Please log in'
 
-
-    if request.method == 'GET':
-        print('get')
+            context.update({
+                'total_price': total_price,
+                'message':message,
+                'items_in_cart': items_in_cart 
+            })
 
     elif request.method == 'POST':
         print('post')
         message = 'Thank you for shopping for us'
-        cart = ()
-        request.session.modified = True
+        
+        for key in cart:
+            query = model.objects.get(pk = key)
+            checkout_price += query.price
+            items_in_cart.append(query)
+        
         items_in_cart = []
+        request.session['cart'] = ()
+        request.session.modified = True
+    
+        context.update({
+            'checkout_price': checkout_price,
+            'message':message,
+            'items_in_cart': items_in_cart 
+        })
 
-    context = {
-        'Item':model,
-        'items_in_cart': items_in_cart,
-        'total_price': total_price,
-        'message':message,
-    }
-
-    print(context['total_price'])
     return render(request, template_name, context)
